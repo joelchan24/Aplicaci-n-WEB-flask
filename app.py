4e8398db4fa82e0f3cfca81b38beee7ffc17735c
Juan <juanbaas48@hotmail.com>
2019-11-02 11:43:07 -0600

    make it better

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 #Para crear nuestras rutas del servidor o url
 app = Flask(__name__)
-PORT = 5000
+#PORT = 5000
 
 #Decolorador
 #Metodo route recibe un nombre para poder crear una url
@@ -28,4 +28,4 @@ def footer():
 
 if __name__ == '__main__':
     #app.debug = True
-    app.run(port = PORT)
+    app.run()
