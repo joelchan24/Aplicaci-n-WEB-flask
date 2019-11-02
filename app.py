bceff574cc783012ab02ace710f40370324c6e29
Juan <juanbaas48@hotmail.com>
2019-11-02 10:35:55 -0600

    make it better

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+#Para crear nuestras rutas del servidor o url
+app = Flask(__name__)
+
+#Decolorador
+#Metodo route recibe un nombre para poder crear una url
+@app.route('/')
+def Login():
+    return render_template('Login.html')
+
+@app.route('/templates/index.html')
+def home():
+    return render_template('index.html')
+
+@app.route('/templates/pages-profile.html')
+def profile():
+    return render_template('pages-profile.html')
+
+@app.route('/templates/table-basic.html')
+def table():
+    return render_template('table-basic.html')
+
+@app.route('/templates/footer.html')
+def footer():
+    return render_template('footer.html')
+    
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
