1af36826c9aa835c0e41e2cd68ccb315191e9486
Juan <juanbaas48@hotmail.com>
2019-11-29 10:43:25 -0600

    make it better

--- app.py
+++ app.py
@@ -1,30 +1,62 @@
-from flask import Flask, render_template
-#Para crear nuestras rutas del servidor o url
-app = Flask(__name__)
-#PORT = 5000
+from flask import Blueprint, Flask, render_template,request, redirect, url_for, flash
+import requests
+from datetime import datetime, date
 
+
+
+#Para crear nuestras rutas del servidor o url
+app = Flask(__name__,instance_relative_config=True)
+# settings
+app.secret_key = "mysecretkey"
 #Decolorador
 #Metodo route recibe un nombre para poder crear una url
 @app.route('/', methods = ['GET'])
 def Login():
     return render_template('Login.html')
 
-@app.route('/templates/index.html')
+
+@app.route('/index.html')
 def home():
-    return render_template('index.html')
+    URL = "https://api-beesoft.herokuapp.com/GetAllUsers"   
+    r = requests.get(url = URL)    
+    data = r.json()
 
-@app.route('/templates/pages-profile.html')
+    URL = "https://api-beesoft.herokuapp.com/GetAllTables"   
+    r = requests.get(url = URL)    
+    data2 = r.json()
+
+    URL = "https://api-beesoft.herokuapp.com/GetAllCajas"   
+    r = requests.get(url = URL)    
+    data3 = r.json()
+
+    URL = "https://api-beesoft.herokuapp.com/GetAllUsers"   
+    r = requests.get(url = URL)    
+    data4 = r.json()    
+    return render_template('index.html',data=data, data2=data2, data3=data3, data4=data4)
+
+
+@app.route('/Perfil.html')
 def profile():
-    return render_template('pages-profile.html')
+    return render_template('Perfil.html')  
+@app.route('/Perfil/<idusuario>')
+def Perfil(idusuario):
+    URL = "https://api-beesoft.herokuapp.com/perfil" 
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}  
+    PARAMS = {'idUsuario':idusuario}
+    r = requests.post(url = URL, json= PARAMS,headers=headers)    
+    data = r.json()
+    print(data)
+    return render_template('Perfil.html',data=data)  
 
-@app.route('/templates/table-basic.html')
-def table():
-    return render_template('table-basic.html')
+@app.route('/CrontrolUsuarios.html')
+def CrontrolUsuarios():
+    URL = "https://api-beesoft.herokuapp.com/listAllUsers"   
+   
+    r = requests.get(url = URL)    
+   
+    data = r.json()    
+    return render_template('CrontrolUsuarios.html',data=data)
 
-@app.route('/templates/footer.html')
-def footer():
-    return render_template('footer.html')
-    
 
 if __name__ == '__main__':
     #app.debug = True
